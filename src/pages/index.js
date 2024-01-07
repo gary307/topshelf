016845ad11c31e568063c6ce06705d1df3fa0527
garyb-harrys <gary.butler@harrys.com>
2024-01-07 14:40:17 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,58 @@
-import * as React from "react"
+import * as React from "react";
+import premium_1 from "../videos/premium_1.mp4";
+import { styled, createGlobalStyle } from "styled-components";
 
-const pageStyles = {
+const GlobalTemplateStyle = createGlobalStyle`
+
+body {
+  font-family: 'Avenir', arial, sans-serif;
+  background: black;
+  color: white;
+}
+`;
+
+const VideoContainer = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  width: 590px;
+  height: 180px;
+  border-radius: 500px;
+  position: relative;
+`;
+
+const StyledVideo = styled.video`
+  margin-top: -147px;
+  margin-left: -30px;
+  position: absolute;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+  font-family: arial;
+`;
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const ModuleContainer = styled.div`
+  display: flex;
+  gap: 50px;
+  margin: 100px 0;
+  position: relative;
+  box-sizing: border-box;
+  align-items: center;
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
+`;
+const ModuleColumn = styled.div`
+  width: 50%;
+  box-sizing: border-box;
+`;
 
 const links = [
   {
@@ -121,56 +98,75 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
-]
+];
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    <>
+      <GlobalTemplateStyle />
+      <Wrapper>
+        <ModuleContainer>
+          <ModuleColumn>
+            <h2>Title</h2>
 
-export default IndexPage
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              venenatis odio et vestibulum tempus. Fusce orci eros, finibus at
+              mi non, tristique porta ipsum. Aenean non ligula imperdiet,
+              gravida diam vulputate, tincidunt tellus. Morbi ex lorem, placerat
+              sed lorem nec, luctus luctus nulla. Aliquam tempor ante nisi, at
+              imperdiet dui ultrices eget
+            </p>
+          </ModuleColumn>
 
-export const Head = () => <title>Home Page</title>
+          <ModuleColumn>
+            <VideoContainer>
+              <StyledVideo
+                width="640"
+                height="480"
+                controls={false}
+                src={premium_1}
+                loop
+                autoPlay
+                muted
+              />
+            </VideoContainer>
+          </ModuleColumn>
+        </ModuleContainer>
+
+        <ModuleContainer>
+          <ModuleColumn>
+            <VideoContainer>
+              <StyledVideo
+                width="640"
+                height="480"
+                controls={false}
+                src={premium_1}
+                loop
+                autoPlay
+                muted
+              />
+            </VideoContainer>
+          </ModuleColumn>
+
+          <ModuleColumn>
+            <h2>Title</h2>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              venenatis odio et vestibulum tempus. Fusce orci eros, finibus at
+              mi non, tristique porta ipsum. Aenean non ligula imperdiet,
+              gravida diam vulputate, tincidunt tellus. Morbi ex lorem, placerat
+              sed lorem nec, luctus luctus nulla. Aliquam tempor ante nisi, at
+              imperdiet dui ultrices eget
+            </p>
+          </ModuleColumn>
+        </ModuleContainer>
+      </Wrapper>
+    </>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
